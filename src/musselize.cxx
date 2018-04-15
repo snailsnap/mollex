1ed5256f5e932260abf2da3ba659adeb71d6e3e5
Tobias Markus <tobias@miglix.eu>
2018-04-15 12:52:33 +0200

    musselize: add some processing

--- src/musselize.cxx
+++ src/musselize.cxx
@@ -2,12 +2,21 @@
 #include <iostream>
 
 #include <opencv/cv.hpp>
+//#include <opencv/highgui.hpp>
 
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "usage: %s <image>\n", argv[0]);
     }
-    cv::Mat img = cv::imread(argv[1]);
+
+    cv::namedWindow("out", cv::WINDOW_NORMAL | cv::WINDOW_GUI_NORMAL);
+    cv::resizeWindow("out", 640, 480);
+
+    cv::Mat tmp, img = cv::imread(argv[1]);
+    cv::compare(img, 128, tmp, cv::CMP_GE);
+
+    cv::imshow("out", tmp);
+    cv::waitKey(0);
 
     return EXIT_SUCCESS;
 }
