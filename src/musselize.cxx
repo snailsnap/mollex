2a1b921c8829398b3a4f43e50e42533ffda4d66f
Tobias Markus <tobias@miglix.eu>
2018-04-15 12:29:11 +0200

    musselize: Add newline to help output

--- src/musselize.cxx
+++ src/musselize.cxx
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "usage: %s <image>", argv[0]);
+        fprintf(stderr, "usage: %s <image>\n", argv[0]);
     }
     cv::Mat img = cv::imread(argv[1]);
 
