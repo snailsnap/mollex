b65ab7fcf4ad346d1d131d8c21a0ca1fe0b3d48a
Tobias Markus <tobias@miglix.eu>
2018-04-15 20:14:23 +0200

    musselize: first workable version
    * Algorithmic improvements make this version viable for extended testing
    * Reorganize (and modularize) the processing
    * Add some tunables

--- src/musselize.cxx
+++ src/musselize.cxx
@@ -6,42 +6,100 @@
 #include <opencv/cv.hpp>
 //#include <opencv/highgui.hpp>
 
+// ### TUNABLES ###
+/*
+ * Boxiness is the area of the fitted rectangle divided through
+ * the area of the actual contour.
+ * A low boxiness value (near 1.0) indicates a very box-like shape.
+ * A high boxiness value (greater than 2.0) indicates a very chaotic
+ * shape that we are not interested in.
+ */
+#define BOXINESS_CUTOFF_LO 1.18d
+#define BOXINESS_CUTOFF_HI 1.5d
+/*
+ * The minimum area occupied by a possible mollusc.
+ */
+#define MINIMUM_AREA 1e5
+
 bool decide(std::vector<cv::Point2i> cont) {
     const double area = cv::contourArea(cont);
-    const cv::Rect brect = cv::boundingRect(cont);
-    const double brect_area = brect.area();
+    const cv::RotatedRect brect = cv::minAreaRect(cont);
+    const double brect_area = brect.size.area();
     const double quotient = brect_area/area;
+    if (area < MINIMUM_AREA) {
+        return true;
+    }
     std::cout << area << " b:" << brect_area << " " << quotient;
-        if (cont.size() >= 5 && 0) {
-            const cv::RotatedRect ell = cv::fitEllipse(cont);
-            const double ell_area = ell.size.area();
-            std::cout << " " << ell_area << " " << ell_area/area;
-        }
-        if (area < 1e5) {
-            std::cout << " ignore" << std::endl;
-            return true;
-        }
-        std::cout << std::endl;
-    return quotient < 1.15;
+#if 0
+    if (cont.size() >= 5 && 0) {
+        const cv::RotatedRect ell = cv::fitEllipse(cont);
+        const double ell_area = ell.size.area();
+        std::cout << " " << ell_area << " " << ell_area/area;
+    }
+#endif
+    std::cout << std::endl;
+    return quotient < BOXINESS_CUTOFF_LO ||
+           quotient > BOXINESS_CUTOFF_HI;
+}
+
+std::vector<std::vector<cv::Point2i>> find_contours(const cv::Mat& in) {
+    std::vector<std::vector<cv::Point2i>> contours;
+    cv::findContours(in, contours, cv::RETR_LIST, cv::CHAIN_APPROX_TC89_L1);
+    contours.erase(std::remove_if(contours.begin(), contours.end(), decide), contours.end());
+    std::cout << "Found " << contours.size() << " potential molluscs" << std::endl;
+
+    return contours;
 }
 
 void denoise(const cv::Mat& in, cv::Mat& out) {
-    const cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
-    const cv::Point2i anchor { -1, -1 };
+    cv::Mat tmp, blurred, hist;
+ 
+    cv::GaussianBlur(in, blurred, cv::Size(3, 3), 0);
+    cv::pyrDown(blurred, blurred);
+    cv::calcHist(std::vector<cv::Mat>{blurred}, std::vector<int>{0},
+        cv::Mat(), hist, std::vector<int>{64}, std::vector<float>{0.0, 1.0});
+        std::cout << hist << std::endl;
 
-    cv::Mat tmp;
-    cv::blur(in, tmp, cv::Size(3, 3));
-    cv::threshold(tmp, tmp, 0.22, 1.0, cv::THRESH_TOZERO);
-    tmp.convertTo(out, CV_8UC1, 255.0);
+    int max_idx = 0, max = 0;
+    for (int i = 0; i < 32; i++) {
+        if (hist.at<float>(i) > max) {
+            max_idx = i;
+            max = hist.at<float>(i);
+        }
+    }
+
+    const int threshold = ((max_idx << 2) - 4) / 32.0;
+    std::cout <<  "thr:" << threshold << std::endl;
+    cv::threshold(in, tmp, threshold, 1.0, cv::THRESH_TOZERO);
+    in.convertTo(out, CV_8UC1, 255.0);
     cv::compare(out, 0, out, cv::CMP_GT);
 }
 
-void process(const char* img_fname) {
-    const cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
+cv::Mat get_structuring_element(const int order) {
+    const int ksize = (order << 1) + 1;
+    return cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(ksize, ksize));
+}
+
+void morphological_filtering(cv::Mat& img) {
+    const cv::Mat se { get_structuring_element(3) };
     const cv::Point2i anchor { -1, -1 };
+#if 0
+    for (int i = 0; i < 10; i++) {
+        const cv::Mat se = get_structuring_element(i);
+        cv::morphologyEx(tmp2, tmp2, cv::MORPH_OPEN, se, anchor, 1);
+    }
 
+    for (int i = 0; i < 10; i++) {
+        const cv::Mat se = get_structuring_element(i);
+        cv::morphologyEx(tmp2, tmp2, cv::MORPH_CLOSE, se, anchor, 1);
+    }
+#endif
+    cv::morphologyEx(img, img, cv::MORPH_OPEN, se, anchor, 8);
+    cv::morphologyEx(img, img, cv::MORPH_CLOSE, se, anchor, 2);
+//  cv::morphologyEx(img, imt, cv::MORPH_GRADIENT, se, anchor, 1);
+}
 
-
+void process(const char* img_fname) {
     cv::namedWindow("in", cv::WINDOW_NORMAL | cv::WINDOW_GUI_NORMAL);
     cv::resizeWindow("in", 640, 480);
     cv::namedWindow("out", cv::WINDOW_NORMAL | cv::WINDOW_GUI_NORMAL);
@@ -50,25 +108,23 @@ void process(const char* img_fname) {
     cv::resizeWindow("eroded", 640, 480);
 
     cv::Mat tmp, tmp2, tmp3, img = cv::imread(img_fname);
+    cv::Mat hsv[3];
     img.convertTo(tmp, CV_32FC3, 1/255.0);
-    cv::cvtColor(tmp, tmp, cv::COLOR_RGB2GRAY, 1);
+    cv::cvtColor(tmp, tmp, cv::COLOR_BGR2HSV, 3);
+    cv::split(tmp, hsv);
 
-    denoise(tmp, tmp2);
+    denoise(hsv[1], tmp2);
+    cv::imshow("out", tmp2);
+//  cv::Canny(tmp2, tmp2, 25, 40);
 
-    cv::morphologyEx(tmp2, tmp2, cv::MORPH_OPEN, se, anchor, 5);
-    cv::morphologyEx(tmp2, tmp2, cv::MORPH_CLOSE, se, anchor, 7);
+    morphological_filtering(tmp2);
 
-    std::vector<std::vector<cv::Point2i>> contours;
-    cv::Mat t;
-    cv::findContours(tmp2, contours, t, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-    contours.erase(std::remove_if(contours.begin(), contours.end(), decide), contours.end());
-    std::cout << contours.size() << std::endl;
+    std::vector<std::vector<cv::Point2i>> contours { find_contours(tmp2) };
 
     const cv::Scalar red { 0, 0, 255 };
     cv::drawContours(img, contours, -1, red, 3);
 
     cv::imshow("in", img);
-    cv::imshow("out", tmp);
     cv::imshow("eroded", tmp2);
     cv::waitKey(0);
 }
