6395bd0e45af8b67b748c30d0f8db0eb7ca3c791
Tobias Markus <tobias@miglix.eu>
2018-04-15 14:54:00 +0200

    musselize: filter and draw contours

--- src/musselize.cxx
+++ src/musselize.cxx
@@ -1,14 +1,46 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include <opencv/cv.hpp>
 //#include <opencv/highgui.hpp>
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <image>\n", argv[0]);
-    }
+bool decide(std::vector<cv::Point2i> cont) {
+    const double area = cv::contourArea(cont);
+    const cv::Rect brect = cv::boundingRect(cont);
+    const double brect_area = brect.area();
+    const double quotient = brect_area/area;
+    std::cout << area << " b:" << brect_area << " " << quotient;
+        if (cont.size() >= 5 && 0) {
+            const cv::RotatedRect ell = cv::fitEllipse(cont);
+            const double ell_area = ell.size.area();
+            std::cout << " " << ell_area << " " << ell_area/area;
+        }
+        if (area < 1e5) {
+            std::cout << " ignore" << std::endl;
+            return true;
+        }
+        std::cout << std::endl;
+    return quotient < 1.15;
+}
+
+void denoise(const cv::Mat& in, cv::Mat& out) {
+    const cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
+    const cv::Point2i anchor { -1, -1 };
+
+    cv::Mat tmp;
+    cv::blur(in, tmp, cv::Size(3, 3));
+    cv::threshold(tmp, tmp, 0.22, 1.0, cv::THRESH_TOZERO);
+    tmp.convertTo(out, CV_8UC1, 255.0);
+    cv::compare(out, 0, out, cv::CMP_GT);
+}
+
+void process(const char* img_fname) {
+    const cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
+    const cv::Point2i anchor { -1, -1 };
+
+
 
     cv::namedWindow("in", cv::WINDOW_NORMAL | cv::WINDOW_GUI_NORMAL);
     cv::resizeWindow("in", 640, 480);
@@ -17,15 +49,11 @@ int main(int argc, char **argv) {
     cv::namedWindow("eroded", cv::WINDOW_NORMAL | cv::WINDOW_GUI_NORMAL);
     cv::resizeWindow("eroded", 640, 480);
 
-    cv::Mat tmp, tmp2, tmp3, img = cv::imread(argv[1]);
+    cv::Mat tmp, tmp2, tmp3, img = cv::imread(img_fname);
     img.convertTo(tmp, CV_32FC3, 1/255.0);
     cv::cvtColor(tmp, tmp, cv::COLOR_RGB2GRAY, 1);
-    cv::blur(tmp, tmp, cv::Size(5, 5));
-    cv::threshold(tmp, tmp, 0.26, 1.0, cv::THRESH_TOZERO);
-    const cv::Mat se = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6));
-    const cv::Point2i anchor { -1, -1 };
-    tmp.convertTo(tmp2, CV_8UC1, 255.0);
-    cv::compare(tmp2, 0, tmp2, cv::CMP_GT);
+
+    denoise(tmp, tmp2);
 
     cv::morphologyEx(tmp2, tmp2, cv::MORPH_OPEN, se, anchor, 5);
     cv::morphologyEx(tmp2, tmp2, cv::MORPH_CLOSE, se, anchor, 7);
@@ -33,17 +61,24 @@ int main(int argc, char **argv) {
     std::vector<std::vector<cv::Point2i>> contours;
     cv::Mat t;
     cv::findContours(tmp2, contours, t, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-    for (const auto& cont: contours) {
-        for (const auto& pt: cont) {
-            std::cout << pt;
-        }
-        std::cout << std::endl;
-    }
+    contours.erase(std::remove_if(contours.begin(), contours.end(), decide), contours.end());
+    std::cout << contours.size() << std::endl;
+
+    const cv::Scalar red { 0, 0, 255 };
+    cv::drawContours(img, contours, -1, red, 3);
 
     cv::imshow("in", img);
     cv::imshow("out", tmp);
     cv::imshow("eroded", tmp2);
     cv::waitKey(0);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <image>\n", argv[0]);
+    }
+
+    process(argv[1]);
 
     return EXIT_SUCCESS;
 }
