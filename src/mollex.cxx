c5edaf73c97f22c8e231874939005ca8ae53a302
Tobias Markus <tobias@miglix.eu>
2018-04-17 16:14:24 +0200

    mollex: improve tunables documentation

--- src/mollex.cxx
+++ src/mollex.cxx
@@ -20,12 +20,31 @@
  * The minimum area occupied by a possible mollusc.
  */
 #define MINIMUM_AREA 1e5
-
+/*
+ * Perform thresholding on the image.
+ * The threshold is determined by considering the image's histogram.
+ */
 #define THRESHOLD
-#define MORPH_SINGLE
-#define MORPH_MULTIPLE
+/*
+ * Perform a single Gaussian blur prefiltering step.
+ * NOTE: This is not known to noticeably improve contour detection
+ * performance, however it is not known to harm either.
+ */
 #define PREFILTER_GAUSSIAN
-
+/*
+ * Perform just one morphological open/close operation using a
+ * fixed structuring element.
+ */
+#define MORPH_SINGLE
+/*
+ * Perform n morphological open/close operations using structuring
+ * elements of order [1;n).
+ */
+#define MORPH_MULTIPLE
+/*
+ * Number of open/close iterations to use with a fixed
+ * structuring element.
+ */
 #define MORPH_SINGLE_OPEN_ITERATIONS 5
 #define MORPH_SINGLE_CLOSE_ITERATIONS 0
 
