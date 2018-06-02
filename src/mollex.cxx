90a7f02551c1b634cfe8ad193f5ccaecfa63927f
Tobias Markus <tobias@miglix.eu>
2018-06-02 23:24:19 +0200

    Fail when the user doesn't provide any filename

--- src/mollex.cxx
+++ src/mollex.cxx
@@ -181,6 +181,7 @@ void process(const char* img_fname) {
 int main(int argc, char **argv) {
     if (argc != 2) {
         std::cerr << "usage: " << argv[0] << " <image>" << std::endl;
+        return EXIT_FAILURE;
     }
 
     process(argv[1]);
